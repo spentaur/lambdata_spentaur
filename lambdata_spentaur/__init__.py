1f4c6004b539b2edef8ee2f7d11df896863cc9e3
Spencer Adams <spencer.adams93@gmail.com>
2019-10-29 22:45:00 -0500

    init

---
+++ lambdata_spentaur/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.0.0.0.1"
+my_str = "this is my_str from __init__.py"
+
+
---
+++ lambdata_spentaur/df_utils.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+
+ONES = pd.Series(np.ones(20))
+ZEROS = pd.Series(np.zeros(20))
