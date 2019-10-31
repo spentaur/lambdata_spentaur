5246674d597ec8aae1ba315a2d36f7e1f8246bfc
Spencer Adams <spencer.adams93@gmail.com>
2019-10-31 00:49:03 -0500

    1.1

--- lambdata_spentaur/__init__.py
+++ lambdata_spentaur/__init__.py
@@ -1,2 +1,1 @@
-__version__ = "0.1.0"
-my_str = "this is my_str from __init__.py"
+__version__ = "0.1.1"
--- lambdata_spentaur/df_utils.py
+++ lambdata_spentaur/df_utils.py
@@ -7,13 +7,15 @@ ZEROS = pd.Series(np.zeros(20))
 # Check a dataframe for nulls, print/report them in a nice "pretty" format
 def perc_missing(X):
     nulls = X.isna().sum()
-    print('% Missing')
+    print("  % Missing  ")
+    print("--------------")
     return (nulls[nulls > 0] / len(X) * 100).sort_values(ascending=False)
 
 def split_date(X, time_intervals):
     '''
     X = DataFrame with Date column
-    time_intervals = array of pandas time date components
+    time_intervals = array of pandas time date components, stylized for the column name
+    (capital and spaced)
     https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#time-date-components
 
     '''
@@ -22,6 +24,6 @@ def split_date(X, time_intervals):
     X['Date'] = pd.to_datetime(X['Date'])
 
     for interval in time_intervals:
-        X[interval] = getattr(X['Date'].dt, interval)
+        X[interval] = getattr(X['Date'].dt, interval.replace(' ', '').lower())
 
     return X
