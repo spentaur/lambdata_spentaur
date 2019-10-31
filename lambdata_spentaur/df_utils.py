87f98afa7c8ad3bc3ba57e707d6c3f170e9ed3c6
Spencer Adams <spencer.adams93@gmail.com>
2019-10-31 00:35:40 -0500

    "adding some functions"

--- lambdata_spentaur/df_utils.py
+++ lambdata_spentaur/df_utils.py
@@ -3,3 +3,25 @@ import pandas as pd
 
 ONES = pd.Series(np.ones(20))
 ZEROS = pd.Series(np.zeros(20))
+
+# Check a dataframe for nulls, print/report them in a nice "pretty" format
+def perc_missing(X):
+    nulls = X.isna().sum()
+    print('% Missing')
+    return (nulls[nulls > 0] / len(X) * 100).sort_values(ascending=False)
+
+def split_date(X, time_intervals):
+    '''
+    X = DataFrame with Date column
+    time_intervals = array of pandas time date components
+    https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#time-date-components
+
+    '''
+    X = X.copy()
+
+    X['Date'] = pd.to_datetime(X['Date'])
+
+    for interval in time_intervals:
+        X[interval] = getattr(X['Date'].dt, interval)
+
+    return X
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lambdata_spentaur",
-    version="0.0.1",
+    version="0.1.0",
     author="spentaur",
     description="A collection of Data Science helper functions",
     long_description=LONG_DESCRIPTION,
